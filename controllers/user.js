c5865b5d17b95fa88faa3f26c565795f91c29930
Gasmi Yassine <gasmi.yassine92@gmail.com>
2024-02-22 17:27:56 +0100

    All errors are managed

--- controllers/user.js
+++ controllers/user.js
@@ -3,22 +3,87 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
+  if (req.body.email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(req.body.email)) {
+      return res.status(400).json({ message: "Adresse e-mail invalide" });
+    }
+  } else {
+    return res
+      .status(400)
+      .json({ message: "Veuillez saisir une adresse e-mail" });
+  }
+
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return res.status(400).json({ message: "Votre mot de passe doit contenir au moins 6 caractères" });
+    }
+
+    if (req.body.password.includes(" ")) {
+      return res
+        .status(400)
+        .json({ message: "Le mot de passe ne peut pas contenir d'espaces" });
+    }
+  }
+  else {
+    return res
+    .status(400)
+    .json({ message: "Veuillez saisir un mot de passe" });
+  }
+
   bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisatuer créé" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  .hash(req.body.password, 10)
+  .then((hash) => {
+    User.findOne({ email: req.body.email })
+      .then((existingUser) => {
+        if (existingUser) {
+          return res.status(400).json({ error: "Cette adresse e-mail est déjà prise." });
+        } else {
+          const user = new User({
+            email: req.body.email,
+            password: hash,
+          });
+          user
+            .save()
+            .then(() => res.status(201).json({ message: "Utilisateur créé" }))
+            .catch((error) => res.status(500).json({ error }));
+        }
+      })
+      .catch((error) => res.status(500).json({ error }));
+  })
+  .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
+ 
+  if (req.body.email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(req.body.email)) {
+      return res.status(400).json({ message: "Adresse e-mail invalide" });
+    }
+  } else {
+    return res
+      .status(400)
+      .json({ message: "Veuillez saisir une adresse e-mail" });
+  }
+
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return res.status(400).json({ message: "Votre mot de passe doit contenir au moins 6 caractères" });
+    }
+
+    if (req.body.password.includes(" ")) {
+      return res
+        .status(400)
+        .json({ message: "Le mot de passe ne peut pas contenir d'espaces" });
+    }
+  }
+  else {
+    return res
+    .status(400)
+    .json({ message: "Veuillez saisir un mot de passe" });
+  }
+
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (user === null) {
@@ -28,8 +93,8 @@ exports.login = (req, res, next) => {
       } else {
         bcrypt
           .compare(req.body.password, user.password)
-          .then((valid) => {
-            if (!valid) {
+          .then((password) => {
+            if (!password) {
               res.status(401).json({
                 message: "La combinaison identifiant/mot de passe est fausse",
               });
@@ -42,7 +107,7 @@ exports.login = (req, res, next) => {
               });
             }
           })
-          .catch((error) => res.status(500).json({ error }));
+          .catch((error) => res.status(400).json({ error }));
       }
     })
     .catch((error) => res.status(500).json({ error }));
